a0556ba9db22397fa865183f078e80a184585029
Burhan Aslam wani <waniburhan@outlook.com>
2018-01-25 13:23:42 +0530

    django ajax

--- Django/mysite/blog/views.py
+++ Django/mysite/blog/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from django.views import generic
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
-
+from django.contrib import messages
 
 def index(request):
     latest_heading_list = Heading.objects.order_by('-pub_date')[:10]
@@ -49,12 +49,47 @@ def detail(request, heading_id):
         #raise Http404("Question does not exist")
         return render(request, 'blog/detail.html', {'heading': heading})
 def ajx(request):
+
     if request.method == "POST":
-        email = request.POST['email']
-        password = request.POST['password']
-        User.objects.create(
-          email = email,
-          password = password
-        )
+
+            #email = request.POST['email']
+            #password = request.POST['password']
+
+        print("fffffffffffffffffffffffffffffffffffffffffffffffffffff")
+        email2 = request.POST['email1']
+        password2 = request.POST['password1']
+        match_email = User.objects.filter(email=email2)
+        match_password = User.objects.filter(password=password2)
+        if match_email and match_password:
+            return HttpResponse("logged in")
+        else:
+            return HttpResponse("DoesNotExist")
+
+        #try:
+
+        match = User.objects.filter(email=email)
+        if match:
+
+            #messages.info(request, 'Your password already exists!')
+            return HttpResponse("Email already exists")
+            #return render('ajx.html', message='Save complete')
+        else:
+            User.objects.create(
+            email = email,
+            password = password
+            )
+
+            return HttpResponse("new user created")
+
+     #except User.DoesNotExist:
+            # Unable to find a user, this is fine
+         #return email
+    #    if User.objects.search(email):
+    #        print('true')
+        #raise Exception('This email address is already in use.')
+
+
+
     return render(request,'blog/ajx.html')
+
 # Create your views here.
