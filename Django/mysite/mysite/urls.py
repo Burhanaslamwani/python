8ba833fe75d178c8f52b08b9c094cd7aac722cf7
Burhan Aslam wani <waniburhan@outlook.com>
2018-02-03 15:27:58 +0530

    django clinical trials search app

--- Django/mysite/mysite/urls.py
+++ Django/mysite/mysite/urls.py
@@ -27,4 +27,3 @@ path('ClinicalTrialSearch/', include('ClinicalTrialSearch.urls')),
 ]
 if settings.DEBUG is True:
     urlpatterns  += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    print(urlpatterns,"ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg")
